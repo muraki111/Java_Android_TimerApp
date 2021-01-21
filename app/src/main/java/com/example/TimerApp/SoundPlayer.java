6d4ce5dcb672a0d0c6f86ea55f30ac8e71dc55b8
muraki111 <g1872030@tcu.ac.jp>
2021-01-21 23:53:43 +0900

    1分未満の時の処理追加(音・プログレスバーを赤に)

--- app/src/main/java/com/example/TimerApp/SoundPlayer.java
+++ app/src/main/java/com/example/TimerApp/SoundPlayer.java
@@ -9,14 +9,19 @@ public class SoundPlayer {
 
     private static SoundPool soundPool;
     private static int alarmSound;
+    private static int littleSound;
 
     public SoundPlayer(Context context) {
 
         soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
 
         alarmSound = soundPool.load(context, R.raw.alarm, 1);
+        littleSound = soundPool.load(context, R.raw.little, 1);
     }
     public void playalarmSound() {
         soundPool.play(alarmSound, 1.0f, 1.0f, 1, 0, 1.0f);
+    }
+    public void playlittleSound() {
+        soundPool.play(littleSound, 1.0f, 1.0f, 1, 0, 1.0f);
     }
 }
